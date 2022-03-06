213355f3a20b8b65134dac27a90e59b847519bbc
sujitIwale <56259235+sujitIwale@users.noreply.github.com>
2022-03-06 17:45:33 +0530

    fix: cart button click event

--- client/src/components/TopBar/TopBar.jsx
+++ client/src/components/TopBar/TopBar.jsx
@@ -15,58 +15,64 @@ const TopBar = () => {
 	};
 	return (
 		<div className={`${styles.top_bar} top-bar-container`}>
-			<div className={styles.filter_options}>
-				<span
-					onClick={() => addFiter('all')}
-					className={`${
-						filteredBy === 'all' ? 'selected' : ''
-					} pointer`}>
-					All
-				</span>
-				<span
-					onClick={() => addFiter('pltoh')}
-					className={`${
-						filteredBy === 'pltoh' ? 'selected' : ''
-					} pointer`}>
-					Prize Low to High
-				</span>
-				<span
-					onClick={() => addFiter('phtol')}
-					className={`${
-						filteredBy === 'phtol' ? 'selected' : ''
-					} pointer`}>
-					Prize High to Low
-				</span>
-				<span
-					onClick={() => addFiter('rhtol')}
-					className={`${
-						filteredBy === 'rhtol' ? 'selected' : ''
-					} pointer`}>
-					Rating High to Low
-				</span>
-				<span
-					onClick={() => addFiter('rltoh')}
-					className={`${
-						filteredBy === 'rltoh' ? 'selected' : ''
-					} pointer`}>
-					Rating Low to High
-				</span>
+			<div className='flex column ma-b-4'>
+				<h3>Sort By</h3>
+				<div className={styles.filter_options}>
+					<span
+						onClick={() => addFiter('all')}
+						className={`${
+							filteredBy === 'all' ? 'selected' : ''
+						} pointer`}>
+						All
+					</span>
+					<span
+						onClick={() => addFiter('pltoh')}
+						className={`${
+							filteredBy === 'pltoh' ? 'selected' : ''
+						} pointer`}>
+						Prize Low to High
+					</span>
+					<span
+						onClick={() => addFiter('phtol')}
+						className={`${
+							filteredBy === 'phtol' ? 'selected' : ''
+						} pointer`}>
+						Prize High to Low
+					</span>
+					<span
+						onClick={() => addFiter('rhtol')}
+						className={`${
+							filteredBy === 'rhtol' ? 'selected' : ''
+						} pointer`}>
+						Rating High to Low
+					</span>
+					<span
+						onClick={() => addFiter('rltoh')}
+						className={`${
+							filteredBy === 'rltoh' ? 'selected' : ''
+						} pointer`}>
+						Rating Low to High
+					</span>
+				</div>
 			</div>
 			<div className={styles.toggle_container}>
-				<span
-					onClick={() => addFiter('veg')}
-					className={`${
-						filteredBy === 'veg' ? 'selected-veg' : ''
-					} pointer`}>
-					Veg
-				</span>
-				<span
-					onClick={() => addFiter('non-veg')}
-					className={`${
-						filteredBy === 'non-veg' ? 'selected-non-veg' : ''
-					} pointer`}>
-					Non Veg
-				</span>
+				<h3 className='flex column ma-b-4'>Categories</h3>
+				<div>
+					<span
+						onClick={() => addFiter('veg')}
+						className={`${
+							filteredBy === 'veg' ? 'selected-veg' : ''
+						} pointer`}>
+						Veg
+					</span>
+					<span
+						onClick={() => addFiter('non-veg')}
+						className={`${
+							filteredBy === 'non-veg' ? 'selected-non-veg' : ''
+						} pointer`}>
+						Non Veg
+					</span>
+				</div>
 			</div>
 		</div>
 	);
